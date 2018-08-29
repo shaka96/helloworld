0f66a246398b3e72ac1626bf9680f0a602da7538
aryan86 <aryan86_2014@163.com>
2018-08-30 02:08:04 +0800

    edit echart data format

--- src/components/charts/echartDemo.vue
+++ src/components/charts/echartDemo.vue
@@ -16,31 +16,69 @@
 
      export default {
         name: 'echarts',
+        props: {
+            option: {
+                type: Object,
+                default(){
+                    return {
+                        title: {
+                            text: 'ECharts 入门示例'
+                        },
+                        tooltip: {},
+                        legend: {
+                            data: ['销量']
+                        },
+                        xAxis: {
+                            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+                        },
+                        yAxis: {},
+                        series: [{
+                            name: '销量',
+                            type: 'bar',
+                            data: [5, 20, 36, 10, 10, 20]
+                        }]
+                    }
+                }
+            }
+        },
         data() {
-            return {}
+            return {
+    
+            }
         },
         mounted() {
             let $echartsDOM = document.getElementById('echarts-dom')
             let myEcharts = echarts.init($echartsDOM)
-            let option = {
-                title: {
-                    text: 'ECharts 入门示例'
-                },
-                tooltip: {},
-                legend: {
-                    data: ['销量']
-                },
-                xAxis: {
-                    data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-                },
-                yAxis: {},
-                series: [{
-                    name: '销量',
-                    type: 'bar',
-                    data: [5, 20, 36, 10, 10, 20]
-                }]
-            }
-            myEcharts.setOption(option)
+            // let option = {
+            //     title: {
+            //         text: 'ECharts 入门示例'
+            //     },
+            //     tooltip: {},
+            //     legend: {
+            //         data: ['销量']
+            //     },
+            //     xAxis: {
+            //         data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+            //     },
+            //     yAxis: {},
+            //     series: [{
+            //         name: '销量',
+            //         type: 'bar',
+            //         data: [5, 20, 36, 10, 10, 20]
+            //     }]
+            // }
+            // console.log('this:', this.$data.option)
+            let option = this.option
+            // try {
+            //     if(options)
+                    myEcharts.setOption(option)
+            // } catch (error) {
+                
+            // }
+            
+        },
+        created: {
+            // ?? myEcharts.setOption(option);
         }
     }
 </script>
