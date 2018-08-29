da577570a0948036094e7980873b92a200c1844b
aryan86 <aryan86_2014@163.com>
2018-08-30 01:09:56 +0800

    add echarts demo

---
+++ src/components/charts/echartDemo.vue
@@ -0,0 +1,46 @@
+<style scoped>
+    .echarts {
+        width: 100%;
+        height: 100%;
+    }
+</style>
+
+<template>
+    <div>
+        <div class="echarts" id="echarts-dom"></div>
+    </div>
+</template>
+
+<script>
+    import echarts from 'echarts'
+
+     export default {
+        name: 'echarts',
+        data() {
+            return {}
+        },
+        mounted() {
+            let $echartsDOM = document.getElementById('echarts-dom')
+            let myEcharts = echarts.init($echartsDOM)
+            let option = {
+                title: {
+                    text: 'ECharts 入门示例'
+                },
+                tooltip: {},
+                legend: {
+                    data: ['销量']
+                },
+                xAxis: {
+                    data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+                },
+                yAxis: {},
+                series: [{
+                    name: '销量',
+                    type: 'bar',
+                    data: [5, 20, 36, 10, 10, 20]
+                }]
+            }
+            myEcharts.setOption(option)
+        }
+    }
+</script>
